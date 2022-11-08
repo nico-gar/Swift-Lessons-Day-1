3a8c2246b3a99a4e288e7e98867baa64cb066167
Nicolas Garaycochea <nicolas.garaycochea@gmail.com>
2022-11-08 09:14:54 -0700

    add more files to play with

---
+++ GettingStarted/GettingStarted/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  GettingStarted
+//
+//  Created by Nicolas Garaycochea on 11/8/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        var names = ["Tammy", "Cole"]
+        names.removeFirst()
+        names.removeFirst()
+        names.removeFirst()
+    }
+
+
+}
+
